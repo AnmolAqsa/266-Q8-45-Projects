fc5ce608a197f6e0b25d604bcab0b3fc99b93f16
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:25:49 -0700

    Add files via upload
    Q8, 45 Projects

---
+++ main.ts
@@ -0,0 +1,20 @@
+/*
+Q8. You should create four lines that look like this:
+
+_______________
+
+console.log(5 + 3)
+
+_______________
+
+Your output should simply be four lines with the number 8 appearing once on each line.
+
+*/
+
+console.log(5+3);
+
+console.log(18-10);
+
+console.log(4*2);
+
+console.log(64/8);
